41368744d8541bffe1f8edf1a68865f76deb1cbf
Kayathri2218 <kaya3kayu2000@gmail.com>
2023-02-16 01:50:49 +0530

    add a javascript file to used statment even or odd review

---
+++ main.js
@@ -0,0 +1,9 @@
+let p =parseInt(prompt("enter number:"));
+
+if (p %2 == 0) {
+    console.log("even number");
+    document.write("even number");
+}else {
+    console.log("odd number");
+    document.write("odd number");
+}
